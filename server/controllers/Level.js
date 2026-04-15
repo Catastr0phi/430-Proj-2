440806fd942e5726640835489d0d8affef3fec29
catastrophi <wlzang2@gmail.com>
2026-04-15 13:04:50 -0400

    level model and controller initial setup

---
+++ server/controllers/Level.js
@@ -0,0 +1,48 @@
+const models = require('../models');
+const Level = models.Level;
+
+const makeLevel = async (req, res) => {
+    if (!req.body.name || !req.body.id){
+        return res.status(400).json({error: 'Level name and ID required!'});
+    }
+
+    const levelData = {
+        name: req.body.name,
+        id: req.body.id,
+        costum: req.body.costum,
+    };
+
+    // Time does not need to be added at creation, but can be
+    if (req.body.time){
+        levelData.time = req.body.time;
+    }
+
+    try {
+        const newLevel = new Level(levelData);
+        await newLevel.save();
+        return res.status(201).json({name: newLevel.name, id: newLevel.id});
+    } catch (err) {
+        console.log(err);
+        if (err.code === 11000){
+            return res.status(400).json({error: 'Level already exists!'});
+        }
+        return res.status(500).json({error: 'An error occured making level'});
+    }
+}
+
+const getLevels = async (req, res) => {
+    try {
+        const query = {owner: req.session.account._id};
+        const docs = await Level.find(query).select('name id custom time').lean().exec();
+
+        return res.json({levels: docs});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({error: 'Error retrieving levels!'});
+    }
+};
+
+module.exports = {
+    makeLevel,
+    getLevels,
+}
---
+++ server/models/Level.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const LevelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    id: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    custom: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    time: {
+        type: Number,
+        required: false,
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'Account',
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+LevelSchema.statics.toAPI = (doc) => ({
+    name: doc.name,
+    id: doc.id,
+    custom: doc.custom,
+    time: doc.time,
+});
+
+const LevelModel = mongoose.model('Level', LevelSchema);
+module.exports = LevelModel;
